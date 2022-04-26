6662bebed85d83437179bff11b02e79140f5821b
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-04-26 16:23:52 +0100

    added 13

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -9,18 +9,25 @@ const createArray = (i,finalArr,numberArray,stringArray) => {
 }
 
 function fizzBuzz() {
-    const numArr = [3,13,5,7]
-    const numArr2 = [13,11]
-    const strArr = ['Fizz','Fezz','Buzz','Bang']
-    const strArr2 = ['Fezz', 'Bong']
-    for(let i = 1; i <= 255; i++){
+    const numArr = [[3,5,7],[11]]
+    const strArr = [['Fizz','Buzz','Bang'],['Bong']]
+    for(let i = 1; i <= 195; i++){
         let finalArr = []
         if (i%11 === 0){
-            createArray(i,finalArr,numArr2,strArr2)
+            createArray(i,finalArr,numArr[1],strArr[1])
         } else {
-            createArray(i,finalArr,numArr,strArr)
+            createArray(i,finalArr,numArr[0],strArr[0])
         }
         finalArr = finalArr.filter(finalArr => finalArr.length>0)
+        if (i%13 === 0){
+            for (j=0; j< finalArr.length; j++){
+                if (finalArr[j].includes('B')){
+                   indexOfB = finalArr.indexOf(finalArr.find(element => element.includes('B')))
+                   finalArr = finalArr.slice(0,indexOfB).concat(['Fezz'],finalArr.slice(indexOfB))
+                   break
+                }
+            }
+        }
         if (i%17 === 0){
             finalArr= finalArr.reverse()
         }
