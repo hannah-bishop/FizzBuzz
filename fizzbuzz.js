b03430366db69217fdb225e94fb1971897930b6d
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-04-26 14:01:26 +0100

    created 2 new arrays for bong

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,14 +4,22 @@ const factorFunc = (i,factor,string) => {
 
 function fizzBuzz() {
     const numArr = [3,13,5,7]
+    const numArr2 = [13,11]
     const strArr = ['Fizz','Fezz','Buzz','Bang']
-    for(let i = 1; i <= 255; i++){
+    const strArr2 = ['Fezz', 'Bong']
+    for(let i = 1; i <= 2431; i++){
         let finalStr = ''
-        if (i%13 === 0 && i%11 === 0 ){
-            finalStr = 'FezzBong'
-        } else if (i%11 === 0) {
-            finalStr = 'Bong'
-        }else {
+        if (i%11 === 0){
+            if (i%17 === 0){
+                for (let j = numArr2.length-1; j>= 0; j--){
+                    finalStr += factorFunc(i,numArr2[j],strArr2[j])
+                }
+            } else {
+                for (let j = 0; j<numArr2.length; j++){
+                    finalStr += factorFunc(i,numArr2[j],strArr2[j])
+                }
+            }
+        } else {
             if (i%17 === 0){
                 for (let j = numArr.length-1; j>= 0; j--){
                     finalStr += factorFunc(i,numArr[j],strArr[j])
