624cf8fbef66f8661dd2cd1b3dc4c501699816ab
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-04-26 11:01:55 +0100

    adding Fizz except now it works

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,6 @@
 function fizzBuzz() {
     for(let i = 1; i <= 100; i++){
-        const newi = i%3 === 0 ? 'Fizz' : 3
+        const newi = i%3 === 0 ? 'Fizz' : i
         console.log(newi)
     }
 }
