746a3975e7085f74de7770f1e63ef3217bec1450
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-04-26 11:59:33 +0100

    FuzzBuzzBangBong

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,21 @@
+const factorFunc = (i,factor,string) => {
+    return i%factor === 0 ? string : ''
+}
+
 function fizzBuzz() {
+    const numArr = [3,5,7]
+    const strArr = ['Fizz','Buzz','Bang']
     for(let i = 1; i <= 100; i++){
-        let numArr = [3,5,7]
-        let strArr = ['Fizz','Buzz','Bang']
         let finalStr = ''
-        const factorFunc = (factor,string) => {
-            const newi = i%factor === 0 ? string : ''
-            finalStr += newi 
-        }
-        for (let j = 0; j<numArr.length; j++){
-            factorFunc(numArr[j],strArr[j])
+        if (i%11 === 0){
+            finalStr = 'Bong'
+        } else {
+            for (let j = 0; j<numArr.length; j++){
+                finalStr += factorFunc(i,numArr[j],strArr[j])
+            }
         }
         finalStr = finalStr.length > 0 ? finalStr : i
         console.log(finalStr)
     }
 }
-
 fizzBuzz()
