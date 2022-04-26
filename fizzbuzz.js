5fa5a7503796da94a8f1b84ac0367a96d21403ed
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-04-26 12:15:36 +0100

    fizzfezzbongbuzzbang

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,13 +3,15 @@ const factorFunc = (i,factor,string) => {
 }
 
 function fizzBuzz() {
-    const numArr = [3,5,7]
-    const strArr = ['Fizz','Buzz','Bang']
+    const numArr = [3,13,5,7]
+    const strArr = ['Fizz','Fezz','Buzz','Bang']
     for(let i = 1; i <= 100; i++){
         let finalStr = ''
-        if (i%11 === 0){
+        if (i%13 === 0 && i%11 === 0 ){
+            finalStr = 'FezzBong'
+        } else if (i%11 === 0) {
             finalStr = 'Bong'
-        } else {
+        }else {
             for (let j = 0; j<numArr.length; j++){
                 finalStr += factorFunc(i,numArr[j],strArr[j])
             }
