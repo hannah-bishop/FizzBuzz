dd78df65285886f5942fff30e33be460085f9d9b
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-04-26 11:12:24 +0100

    FizzBuzz for 3 and 5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,10 @@
 function fizzBuzz() {
     for(let i = 1; i <= 100; i++){
-        const newi = i%3 === 0 ? 'Fizz' : i
-        console.log(newi)
+        const newi3 = i%3 === 0 ? 'Fizz' : ''
+        const newi5 = i%5 === 0 ? 'Buzz' : ''
+        const str = newi3 + newi5
+        const finalStr = str.length > 0 ? str : i
+        console.log(finalStr)
     }
 }
 
