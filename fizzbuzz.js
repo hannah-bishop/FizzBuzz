6d1f45de8742410a3aa59df12d4ab496f10e51b6
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-04-26 15:29:06 +0100

    made more general - still needs work ok 13

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,35 +2,29 @@ const factorFunc = (i,factor,string) => {
     return i%factor === 0 ? string : ''
 }
 
+const createArray = (i,finalArr,numberArray,stringArray) => {
+    for (let j = 0; j<numberArray.length; j++){
+        finalArr.push(factorFunc(i,numberArray[j],stringArray[j]))
+    }
+}
+
 function fizzBuzz() {
     const numArr = [3,13,5,7]
     const numArr2 = [13,11]
     const strArr = ['Fizz','Fezz','Buzz','Bang']
     const strArr2 = ['Fezz', 'Bong']
-    for(let i = 1; i <= 2431; i++){
-        let finalStr = ''
+    for(let i = 1; i <= 255; i++){
+        let finalArr = []
         if (i%11 === 0){
-            if (i%17 === 0){
-                for (let j = numArr2.length-1; j>= 0; j--){
-                    finalStr += factorFunc(i,numArr2[j],strArr2[j])
-                }
-            } else {
-                for (let j = 0; j<numArr2.length; j++){
-                    finalStr += factorFunc(i,numArr2[j],strArr2[j])
-                }
-            }
+            createArray(i,finalArr,numArr2,strArr2)
         } else {
-            if (i%17 === 0){
-                for (let j = numArr.length-1; j>= 0; j--){
-                    finalStr += factorFunc(i,numArr[j],strArr[j])
-                }
-            } else {
-                for (let j = 0; j<numArr.length; j++){
-                    finalStr += factorFunc(i,numArr[j],strArr[j])
-                }
-            }
+            createArray(i,finalArr,numArr,strArr)
         }
-        finalStr = finalStr.length > 0 ? finalStr : i
+        finalArr = finalArr.filter(finalArr => finalArr.length>0)
+        if (i%17 === 0){
+            finalArr= finalArr.reverse()
+        }
+        finalStr = finalArr.length > 0 ? finalArr.join('') : i
         console.log(finalStr)
     }
 }
